4a4c6de4273df2681946808b10e78d345b7d2cf5
Andy <anghy.ruz@gmail.com>
2025-04-26 00:26:34 -0400

    PokemonInfo part of the pokemon done

--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -1,8 +1,10 @@
-import { useParams } from "react-router"
+import { Link, useParams } from "react-router"
 
 import { Chart as ChartJS, Filler, Legend, LineElement, PointElement, RadialLinearScale, Tooltip } from 'chart.js'
 import { Radar } from 'react-chartjs-2'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { PokemonDetails } from "../types";
+import { FaArrowLeft, FaHeart } from "react-icons/fa";
 
 ChartJS.register(
   RadialLinearScale,
@@ -57,37 +59,96 @@ const options = {
 };
 
 function PokemonInfo() {
+
+  const [showShiny, setShowShiny] = useState(false)
+
   const { pokeId } = useParams()
-  console.log(pokeId);
-
-  // useEffect(() => {
-  //   fetch()
-  // }, [])
+  const [pokeInfo, setPokeInfo] = useState<PokemonDetails>()
 
 
-  const stats: Array<string> = ['HP', 'Speed', 'Attack']
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`)
+      .then(async res => await res.json())
+      .then(res => setPokeInfo(res))
+  }, [])
+
+  // console.log(pokeId);
+  console.log(pokeInfo);
+
+  const statsLabels = pokeInfo?.stats.map(stat => stat.stat.name) ?? []
+  const statsData = pokeInfo?.stats.map(stat => stat.base_stat) ?? []
+  // console.log(statsData);
+
+
+  // const stats: Array<string> = ['HP', 'Speed', 'Attack']
 
   const data = {
-    // labels: ['HP', 'Speed', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
-    labels: stats,
+    labels: statsLabels,
     datasets: [
       {
-        label: 'Pokemon',
-        data: [2, 9, 3, 5, 2, 3],
+        label: 'Pokemon', //pokemon name
+        data: statsData,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
       },
     ],
   };
 
+  const backImg = pokeInfo?.sprites.back_default ?? false
+  const pokeID = `#${pokeInfo?.id.toString().padStart(3, '0')}`
+
+
+  const handleShowShiny = () => {
+    setShowShiny(!showShiny)
+  }
+
 
   return (
-    <>
-      <div style={{ fontSize: '20px' }} >PokemonInfo</div>
-      <div className="flex justify-center items-center m-auto mt-20">
-        <Radar options={options} data={data} ></Radar>
+    <div className="w-screen h-screen flex">
+      <Link to='/'>
+        <div className="absolute top-10 left-10 text-2xl text-[#484848] hover:text-[#a8a8a8] cursor-pointer border-2 border-[#484848] hover:border-[#a8a8a8] p-2 rounded-full">
+          <FaArrowLeft />
+        </div>
+      </Link>
+
+
+      {/* img del pokemon ver adelante y atras, #0id, button de agregar a me gusta, abajo button de isShiny y mostrar su forma Shiny */}
+      <div className="flex flex-col gap-5 justify-center items-center w-full h-full">
+        <span className="text-2xl rounded-full w-full max-w-[300px] py-1 px-4 border-2 border-[#282828] cursor-pointer hover:border-amber-400">{pokeID}</span>
+        <div style={backImg ? {} : { pointerEvents: 'none' }} className="cardContainer">
+          <div className="cardContent">
+            <div className="cardFront">
+              {
+                showShiny ?
+                  <img className="animate-[pulse_800ms] [animation-iteration-count:2]" src={pokeInfo?.sprites.front_shiny} alt="" />
+                  :
+                  <img src={pokeInfo?.sprites.front_default} alt={`image of the pokemon ${pokeInfo?.name}`} />
+              }
+            </div>
+            <div className="cardBack">
+              {
+                showShiny ?
+                  <img className="animate-[pulse_800ms] [animation-iteration-count:2]" src={pokeInfo?.sprites.back_shiny} alt="" />
+                  :
+                  <img src={pokeInfo?.sprites.back_default} alt={`image of the pokemon ${pokeInfo?.name}`} />
+              }
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-2 w-full">
+          <button onClick={handleShowShiny} style={showShiny ? { borderColor: '#ffb900', color: '#ffb900' } : {}} className="w-full max-w-[300px] flex justify-center items-center  bg-[#282828] border-2 border-[#282828] p-2 py-3 rounded-md hover:border-amber-600 cursor-pointer shadow-lg hover:text-amber-600 text-white font-semibold hover:text-amber-400-500">Show Shiny</button>
+          <button className="flex justify-center items-center p-2 rounded-lg h-full w-15 cursor-pointer border-2 border-[#383838] text-xl text-[#383838] hover:text-red-500 hover:border-red-500"><FaHeart></FaHeart></button>
+        </div>
       </div>
-    </>
+
+
+      {/* about - stats -  */}
+      <div className="w-full">
+        <div className="flex justify-center items-center m-auto mt-20 max-w-[500px]">
+          <Radar options={options} data={data} ></Radar>
+        </div>
+      </div>
+    </div>
   )
 }
 
